461ebcf3ba33eaca7fe3a3dbf17bdce1cc7c1b51
HuntedRaven7 <128195750+HuntedRaven7@users.noreply.github.com>
2025-04-24 10:16:24 -0400

    Add files via upload

--- nvim/init.lua
+++ nvim/init.lua
@@ -21,6 +21,14 @@ vim.opt.rtp:prepend(lazypath)
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
+function ColorMyPencils(color)
+	color = color or "nordic"
+	vim.cmd.colorscheme(color)
+
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+end
+
 -- Setup lazy.nvim
 require("lazy").setup({
 	spec = {
@@ -31,7 +39,14 @@ require("lazy").setup({
 
 		{ "mfussenegger/nvim-dap" },
 
-		{ "Mofiqul/vscode.nvim" },
+        { 'AlexvZyl/nordic.nvim',
+            lazy = false,
+            opts = {},
+            config = function()
+                ColorMyPencils()
+            end
+
+        },
 
 		{ "lommix/godot.nvim" },
 
@@ -45,6 +60,85 @@ require("lazy").setup({
 			"nvim-lualine/lualine.nvim",
 			dependencies = { "nvim-tree/nvim-web-devicons" },
 		},
+
+        {
+        "erikbackman/brightburn.vim",
+    },
+
+    {
+        "folke/tokyonight.nvim",
+        lazy = false,
+        opts = {},
+        config = function()
+            ColorMyPencils()
+        end
+    },
+    {
+        "ellisonleao/gruvbox.nvim",
+        name = "gruvbox",
+        config = function()
+            require("gruvbox").setup({
+                terminal_colors = true, -- add neovim terminal colors
+                undercurl = true,
+                underline = false,
+                bold = true,
+                italic = {
+                    strings = false,
+                    emphasis = false,
+                    comments = false,
+                    operators = false,
+                    folds = false,
+                },
+                strikethrough = true,
+                invert_selection = false,
+                invert_signs = false,
+                invert_tabline = false,
+                invert_intend_guides = false,
+                inverse = true, -- invert background for search, diffs, statuslines and errors
+                contrast = "", -- can be "hard", "soft" or empty string
+                palette_overrides = {},
+                overrides = {},
+                dim_inactive = false,
+                transparent_mode = false,
+            })
+        end,
+    },
+    {
+        "folke/tokyonight.nvim",
+        config = function()
+            require("tokyonight").setup({
+                -- your configuration comes here
+                -- or leave it empty to use the default settings
+                style = "storm", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+                transparent = true, -- Enable this to disable setting the background color
+                terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
+                styles = {
+                    -- Style to be applied to different syntax groups
+                    -- Value is any valid attr-list value for `:help nvim_set_hl`
+                    comments = { italic = false },
+                    keywords = { italic = false },
+                    -- Background styles. Can be "dark", "transparent" or "normal"
+                    sidebars = "dark", -- style for sidebars, see below
+                    floats = "dark", -- style for floating windows
+                },
+            })
+        end
+    },
+
+    {
+        "rose-pine/neovim",
+        name = "rose-pine",
+        config = function()
+            require('rose-pine').setup({
+                disable_background = true,
+                styles = {
+                    italic = false,
+                },
+            })
+
+            ColorMyPencils();
+        end
+    },
 
 		{
 			"yorickpeterse/nvim-window",
@@ -669,7 +763,7 @@ require("lazy").setup({
 	checker = { enabled = true },
 })
 
-vim.cmd("colorscheme vscode")
+--vim.cmd("colorscheme vscode")
 
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
@@ -726,7 +820,7 @@ require("sidebar-nvim").setup({
 	bindings = nil,
 	open = true,
 	side = "left",
-	initial_width = 23,
+	initial_width = 17,
 	hide_statusline = false,
 	update_interval = 1000,
 	sections = { "datetime", "git", "diagnostics" },
