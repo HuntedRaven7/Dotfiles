5950cedc14c4f81ed53dbca574f25bdc9cea95d8
HuntedRaven7 <128195750+HuntedRaven7@users.noreply.github.com>
2025-05-08 11:33:51 -0400

    Add files via upload

---
+++ nvim/lua/edward/init.lua
@@ -0,0 +1,7 @@
+require("edward.set")
+require("edward.remap")
+require("edward.lazy_init")
+vim.cmd('colorscheme flexoki-dark')
+vim.g.netrw_browse_split = 0
+vim.g.netrw_banner = 0
+vim.g.netrw_winsize = 25
