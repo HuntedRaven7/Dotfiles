53d79523cc892abe5d0a6dd8a3fd52e04610ace6
HuntedRaven7 <128195750+HuntedRaven7@users.noreply.github.com>
2025-08-01 19:49:43 -0400

    Delete nvim directory

--- nvim/lua/edward/init.lua
+++
@@ -1,7 +0,0 @@
-require("edward.set")
-require("edward.remap")
-require("edward.lazy_init")
-vim.g.netrw_browse_split = 0
-vim.g.netrw_banner = 0
-vim.g.netrw_winsize = 25
---vim.cmd.colorscheme("codedark")
