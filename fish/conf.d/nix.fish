d1cb208f44c644ed33f672eed509e42e48aae33f
/home/Raven? <128195750+HuntedRaven7@users.noreply.github.com>
2025-10-24 23:22:18 +0000

    Delete fish directory

--- fish/conf.d/nix.fish
+++
@@ -1,2 +0,0 @@
-
-if test -e /home/edward/.nix-profile/etc/profile.d/nix.fish; . /home/edward/.nix-profile/etc/profile.d/nix.fish; end # added by Nix installer
--- fish/config.fish
+++
@@ -1,9 +0,0 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
-
-function fish_greeting
-end
-
-export GTK_FONT_NAME="JetbrainsMono Nerd Font 11"
-starship init fish | source
